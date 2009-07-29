83c5a0f1cfb26282765bd78ef6bb66e8553280bf
dyhl22 <dyhl22@defcfdf0-7b2f-11de-b038-1b1fdbc84190>
2009-07-29 13:07:54 +0000

    Por hacer: Banco.java
    Por terminar:
    ColaArreglos.java

--- Banco/src/ColaArreglos.java
+++ Banco/src/ColaArreglos.java
@@ -7,12 +7,33 @@
  *
  * @author dvdhl89
  */
-public class ColaArreglos extends ColaAbstracta{
+public class ColaArreglos<E> extends ColaAbstracta {
+
+    private Object[] elementos;
+    private int inicio, fin;
 
     public ColaArreglos(int limite) {
         cantidad = 0;
         this.limite = limite;
+        elementos = new Object[limite];
+        inicio = 0;
+        fin = 0;
+
+    }
+
+    @Override
+    public void agregar(Object dato) {
+        if(inicio < limite) {
+            elementos[fin] = dato;
+            fin++;
+        }
         
+    }
+
+    @Override
+    public Object remover() {
+        if(inicio >= 0)
+            return elementos[fin];
     }
 
 }
