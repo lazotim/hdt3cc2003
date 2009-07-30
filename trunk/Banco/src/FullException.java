ffa3df15d2d5097e3e3a8a4a09c3fe4336c8ed24
dyhl22 <dyhl22@defcfdf0-7b2f-11de-b038-1b1fdbc84190>
2009-07-30 07:34:36 +0000

    Por terminar: Banco.java Exceptions

---
+++ trunk/Banco/src/FullException.java
@@ -0,0 +1,21 @@
+//David Hsieh #08225
+//Hoja de Trabajo #1
+
+/**
+ *
+ * @author David Hsieh
+ */
+public class FullException extends Exception{
+    /**
+     *
+     */
+    public FullException() {}
+
+    /**
+     *
+     * @param msg
+     */
+    public FullException(String msg) {
+        super(msg);
+    }
+}
